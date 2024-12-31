871979a550b94c2ff8f039f80c327faf3e57ef4f
pawelekpaskudzki <jonn.pes@gmail.com>
2024-12-31 11:48:55 +0100

    baner

--- src/App.js
+++ src/App.js
@@ -9,29 +9,37 @@ import './App.css';
 
 function App() {
   return (
-     <Router>
-     <div className="app-container">
-       {/* Lewy panel z linkami */}
-       <nav className="side-nav">
-         <ul>
-           <li><Link to="/">Strona Główna</Link></li>
-           <li><Link to="/all">Wszystkie Memy</Link></li>
-           <li><Link to="/best">Najlepsze Memy</Link></li>
-           <li><Link to="/add">Dodaj</Link></li>
-         </ul>
-       </nav>
+    <Router>
+      <div className="app-container">
 
-       {/* Prawy panel z treścią */}
-       <main className="main-content">
-         <Routes>
-         <Route path="/" element={<Default />} />
-        <Route path="/all" element={<AllMemes />} />
-        <Route path="/best" element={<BestMemes />} />
-        <Route path="/add" element={<AddMeme />} />
-         </Routes>
-       </main>
-     </div>
-   </Router>
+        {/* Baner */}
+        <header className="banner">
+          <h1 id="banner-text">MEMIK</h1>
+        </header>
+
+
+        <div className="content-container">
+          <nav className="side-nav">
+            <ul>
+              <li><Link to="/">Strona Główna</Link></li>
+              <li><Link to="/all">Wszystkie Memy</Link></li>
+              <li><Link to="/best">Najlepsze Memy</Link></li>
+              <li><Link to="/add">Dodaj</Link></li>
+            </ul>
+          </nav>
+
+          {/* Prawy panel z treścią */}
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<Default />} />
+              <Route path="/all" element={<AllMemes />} />
+              <Route path="/best" element={<BestMemes />} />
+              <Route path="/add" element={<AddMeme />} />
+            </Routes>
+          </main>
+        </div>
+      </div>
+    </Router>
   );
 }
 
